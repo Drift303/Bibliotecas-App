71a5c206b024d5b6f3f7ab109727f68500f192a2
Yilin Roux <yilinroux@gmail.com>
2026-06-11 03:01:04 -0600

    fix: remover src de Backend subido por error

--- backend/src/routes/syncRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { syncLoans } = require('../controllers/syncController');
-const authGuard = require('../middlewares/authGuard');
-const saasGuard = require('../middlewares/saasGuard');
-
-router.post('/loans', authGuard, saasGuard, syncLoans);
-
-module.exports = router;
--- backend/src/validators/authValidators.js
+++
@@ -1,8 +0,0 @@
-const { z } = require('zod');
-
-const loginSchema = z.object({
-  identifier: z.string().min(2), // email or cct
-  password: z.string().min(6),
-});
-
-module.exports = { loginSchema };
