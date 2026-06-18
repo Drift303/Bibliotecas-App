00556e30d1ce4320dd7920ce19cbce5753e0dc2d
Yilin Roux <yilinroux@gmail.com>
2026-06-18 03:55:21 -0600

    feat: agregar endpoints PUT y DELETE para libros (borrado logico)

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getBooks, createBook } = require('../controllers/bookController');
+
+const {
+  getBooks,
+  createBook,
+  updateBook,
+  deleteBook,
+} = require('../controllers/bookController');
+
 const authGuard = require('../middlewares/authGuard');
 const saasGuard = require('../middlewares/saasGuard');
 
 router.get('/', authGuard, saasGuard, getBooks);
 router.post('/', authGuard, saasGuard, createBook);
+
+router.put('/:id', authGuard, saasGuard, updateBook);
+router.delete('/:id', authGuard, saasGuard, deleteBook);
 
 module.exports = router;
